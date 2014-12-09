52c17f3c8d47ec813770aa56efdd527a8bf6ab81
Anders Pearson <anders@columbia.edu>
2014-12-09 17:29:09 -0500

    configuring for serving static files off S3

--- pump/settings_staging.py
+++ pump/settings_staging.py
@@ -1,8 +1,9 @@
 # flake8: noqa
 from settings_shared import *
+import os
 
 TEMPLATE_DIRS = (
-    "/var/www/pump/pump/pump/templates",
+    os.path.join(os.path.dirname(__file__), "templates"),
 )
 
 MEDIA_ROOT = '/var/www/pump/uploads/'
@@ -22,10 +23,24 @@ DATABASES = {
     }
 }
 
-COMPRESS_ROOT = "/var/www/pump/pump/media/"
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 STAGING_ENV = True
+
+AWS_STORAGE_BUCKET_NAME = "ccnmtl-pump-static-stage"
+AWS_PRELOAD_METADATA = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'pump.s3utils.CompressorS3BotoStorage'
+S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_URL = STATIC_URL
+DEFAULT_FILE_STORAGE = 'pump.s3utils.MediaRootS3BotoStorage'
+MEDIA_URL = S3_URL + '/media/'
+COMPRESS_STORAGE = 'pump.s3utils.CompressorS3BotoStorage'
+AWS_QUERYSTRING_AUTH = False
 
 STATSD_PREFIX = 'pump-staging'
 
