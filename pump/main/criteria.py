1f1d3249625a77e3088fd45369afce6371941f4c
Anders Pearson <anders@columbia.edu>
2015-02-20 14:56:08 +0100

    simple business logic objects

---
+++ pump/main/criteria.py
@@ -0,0 +1,32 @@
+class Houghton(object):
+    """ Houghton PASS is 8 or greater (out of possible 18) """
+    def __init__(self, responses):
+        self.responses = responses
+
+    def score(self):
+        return sum(self.responses)
+
+    def pass_fail(self):
+        return self.score() >= 8
+
+
+class ABC(object):
+    """ ABC PASS is 117 or greater
+    (out of possible 160, using 10pt scale for each of
+    the 16 questions) (which should be 65% or greater) """
+    def __init__(self, responses):
+        self.responses = responses
+
+    def score(self):
+        return sum(self.responses)
+
+    def pass_fail(self):
+        return self.score() >= 117
+
+
+class PassFail(object):
+    def __init__(self, response):
+        self.response = response
+
+    def pass_fail(self):
+        return self.response == 2
---
+++ pump/main/tests/test_criteria.py
@@ -0,0 +1,61 @@
+from unittest import TestCase
+from pump.main.criteria import Houghton, ABC, PassFail
+
+
+class TestHoughton(TestCase):
+    def test_normal(self):
+        h = Houghton([1, 2, 3, 1, 1, 1])
+        self.assertEqual(h.score(), 9)
+        self.assertTrue(h.pass_fail())
+
+    def test_fail(self):
+        h = Houghton([1, 1, 1, 1, 1, 1])
+        self.assertEqual(h.score(), 6)
+        self.assertFalse(h.pass_fail())
+
+    def test_pass(self):
+        h = Houghton([4, 4, 4, 2, 2, 2])
+        self.assertEqual(h.score(), 18)
+        self.assertTrue(h.pass_fail())
+
+    def test_barely(self):
+        h = Houghton([1, 2, 2, 1, 1, 1])
+        self.assertEqual(h.score(), 8)
+        self.assertTrue(h.pass_fail())
+
+
+class TestABC(TestCase):
+    def test_normal(self):
+        a = ABC([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+        self.assertEqual(a.score(), 136)
+        self.assertTrue(a.pass_fail())
+
+    def test_fail(self):
+        a = ABC([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        self.assertEqual(a.score(), 16)
+        self.assertFalse(a.pass_fail())
+
+    def test_high_pass(self):
+        a = ABC([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+                 10, 10, 10, 10, 10])
+        self.assertEqual(a.score(), 160)
+        self.assertTrue(a.pass_fail())
+
+    def test_barely(self):
+        a = ABC([8, 8, 8, 8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7])
+        self.assertEqual(a.score(), 116)
+        self.assertFalse(a.pass_fail())
+
+        a = ABC([8, 8, 8, 8, 8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7])
+        self.assertEqual(a.score(), 117)
+        self.assertTrue(a.pass_fail())
+
+
+class TestPassFail(TestCase):
+    def test_pass(self):
+        p = PassFail(2)
+        self.assertTrue(p.pass_fail())
+
+    def test_fail(self):
+        p = PassFail(1)
+        self.assertFalse(p.pass_fail())
