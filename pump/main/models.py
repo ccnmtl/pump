49608678b0cab549ff3fdee68f0390a6c26479a6
Anders Pearson <anders@columbia.edu>
2015-02-20 15:31:39 +0100

    calculate scores from a response

--- pump/main/models.py
+++ pump/main/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from .criteria import Houghton, ABC, PassFail
 
 
 class Response(models.Model):
     added = models.DateTimeField(auto_now_add=True)
+    # Houghton
     q1 = models.TextField(blank=True, default="")
     q2 = models.TextField(blank=True, default="")
     q3 = models.TextField(blank=True, default="")
     q4 = models.TextField(blank=True, default="")
     q5 = models.TextField(blank=True, default="")
     q6 = models.TextField(blank=True, default="")
+    # ABC
     q7 = models.TextField(blank=True, default="")
     q8 = models.TextField(blank=True, default="")
     q9 = models.TextField(blank=True, default="")
@@ -25,8 +28,38 @@ class Response(models.Model):
     q20 = models.TextField(blank=True, default="")
     q21 = models.TextField(blank=True, default="")
     q22 = models.TextField(blank=True, default="")
+    # pick up
     q23 = models.TextField(blank=True, default="")
+    # look behind
     q24 = models.TextField(blank=True, default="")
 
     def __unicode__(self):
         return "Response %s" % self.added
+
+    def results(self):
+        s = Scorer(self)
+        return s.results()
+
+
+class Scorer(object):
+    def __init__(self, r):
+        h_values = [r.q1, r.q2, r.q3, r.q4, r.q5, r.q6]
+        h_values = [int(v) for v in h_values if v != ""]
+        self.h = Houghton(h_values)
+        a_values = [
+            r.q7, r.q8, r.q9, r.q10, r.q11, r.q12,
+            r.q13, r.q14, r.q15, r.q16, r.q17, r.q18,
+            r.q19, r.q20, r.q21, r.q22,
+        ]
+        a_values = [int(v) for v in a_values if v != ""]
+        self.abc = ABC(a_values)
+        self.pick_up = PassFail(int(r.q23 or '1'))
+        self.look_behind = PassFail(int(r.q24 or '1'))
+
+    def results(self):
+        return dict(
+            houghton=self.h,
+            abc=self.abc,
+            pick_up=self.pick_up,
+            look_behind=self.look_behind,
+        )
