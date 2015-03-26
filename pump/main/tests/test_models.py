88670c43adb57944cee3fbfa3eade190c85e9a60
Anders Pearson <anders@columbia.edu>
2015-03-26 11:18:21 +0100

    :bug: correct context for email template. PMT #100044

--- pump/main/tests/test_models.py
+++ pump/main/tests/test_models.py
@@ -48,3 +48,20 @@ class ResponseTest(TestCase):
     def test_email_text(self):
         r = ResponseFactory()
         self.assertTrue("your results" in r.email_text())
+        self.assertTrue("Houghton: FAIL (0/18)" in r.email_text())
+
+    def test_email_text_pass(self):
+        r = ResponseFactory(
+            # Houghton
+            q1="4", q2="4", q3="4", q4="2", q5="2", q6="2",
+            # ABC
+            q7="10", q8="10", q9="10", q10="10", q11="10",
+            q12="10", q13="10", q14="10", q15="10", q16="10",
+            q17="10", q18="10", q19="10", q20="10", q21="10",
+            q22="10",
+            # pick up
+            q23="2",
+            # look behind
+            q24="2",
+        )
+        self.assertTrue("Houghton: PASS (18/18)" in r.email_text())
