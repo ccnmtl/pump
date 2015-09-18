32e5abf61dd3d95b501b4c620ea82ee9a5b1be24
Anders Pearson <anders@columbia.edu>
2015-09-18 14:19:23 +0200

    convert to ccnmtlsettings

--- pump/settings_staging.py
+++ pump/settings_staging.py
@@ -1,53 +1,19 @@
 # flake8: noqa
 from settings_shared import *
+from ccnmtlsettings.staging import common
 import os
 
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
+project = 'pump'
+base = os.path.dirname(__file__)
 
-MEDIA_ROOT = '/var/www/pump/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/pump/pump/sitemedia'),
-)
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pump',
-        'HOST': '',
-        'PORT': 6432,
-        'USER': '',
-        'PASSWORD': '',
-    }
-}
-
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-STAGING_ENV = True
-
-AWS_S3_CUSTOM_DOMAIN = "dp909d54wadkp.cloudfront.net"
-
-AWS_STORAGE_BUCKET_NAME = "ccnmtl-pump-static-stage"
-AWS_PRELOAD_METADATA = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
-S3_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
-STATIC_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
-COMPRESS_ROOT = STATIC_ROOT
-COMPRESS_URL = STATIC_URL
-DEFAULT_FILE_STORAGE = 'cacheds3storage.MediaRootS3BotoStorage'
-MEDIA_URL = S3_URL + '/media/'
-COMPRESS_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
-AWS_QUERYSTRING_AUTH = False
-
-STATSD_PREFIX = 'pump-staging'
-
-if 'migrate' not in sys.argv:
-    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+locals().update(
+    common(
+        project=project,
+        base=base,
+        STATIC_ROOT=STATIC_ROOT,
+        INSTALLED_APPS=INSTALLED_APPS,
+        cloudfront="dp909d54wadkp",
+    ))
 
 try:
     from local_settings import *
